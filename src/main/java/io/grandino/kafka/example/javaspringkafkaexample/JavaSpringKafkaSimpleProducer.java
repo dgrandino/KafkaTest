0ef59b6baf7ec4574c45fd06b4902e8d766da76d
dgrandino <danielgrandino@gmail.com>
2021-08-16 01:13:58 -0300

    Projeto teste Kafka

---
+++ src/main/java/io/grandino/kafka/example/javaspringkafkaexample/JavaSpringKafkaSimpleProducer.java
@@ -0,0 +1,53 @@
+package io.grandino.kafka.example.javaspringkafkaexample;
+
+import java.util.Properties;
+import java.util.Scanner;
+import java.util.concurrent.Future;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//@SpringBootApplication
+public class JavaSpringKafkaSimpleProducer {
+
+	public static void main(String[] args) throws Exception{
+		//SpringApplication.run(JavaSpringKafkaSimpleProducer.class, args);
+
+		if(args.length!=1){
+			System.err.println("Please specify 1 parameters: topic-name");
+			System.exit(-1);
+		}
+		String topicName = args[0];
+		System.out.println("Enter message(type exit to quit)");
+
+		//Configure the Producer
+		Properties configProperties = new Properties();
+		configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,  StringSerializer.class);
+
+		try(Scanner in = new Scanner(System.in);
+				Producer<String,String> producer = new KafkaProducer<>(configProperties)){
+			String line;
+			do{
+				line=in.nextLine();
+				ProducerRecord<String,String> rec=new ProducerRecord<String,String>(topicName,line);
+				Future<RecordMetadata> results = producer.send(rec);
+				RecordMetadata metadata= results.get();
+				System.out.println("Message sent on particion="  + metadata.partition() + " with offset="+metadata.offset());
+
+			} while (!line.equals("exit"));
+			System.out.print("Exiting...");
+		}
+	}
+
+}
---
+++ src/test/java/io/grandino/kafka/example/javaspringkafkaexample/JavaSpringKafkaSimpleProducerTests.java
@@ -0,0 +1,13 @@
+package io.grandino.kafka.example.javaspringkafkaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringKafkaSimpleProducerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
